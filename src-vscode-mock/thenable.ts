d243a66593502555f30c1294b042a989c5a06e3f
Jan Koehnlein <jan.koehnlein@typefox.io>
2017-12-18 09:57:48 +0100

    added copyright headers

--- src-vscode-mock/thenable.ts
+++ src-vscode-mock/thenable.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2017 TypeFox and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 export interface Thenable<T> {
 	then<TResult>(onfulfilled?: (value: T) => TResult | Thenable<TResult>, onrejected?: (reason: any) => TResult | Thenable<TResult>): Thenable<TResult>;
 	then<TResult>(onfulfilled?: (value: T) => TResult | Thenable<TResult>, onrejected?: (reason: any) => void): Thenable<TResult>;
